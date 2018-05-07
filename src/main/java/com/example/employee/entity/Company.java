27a12ace2e15562fa5ca8e452313746bbbd8cdaa
FeiXie-Liam <xiefei0887@163.com>
2018-05-07 15:24:44 +0800

    finish all test.

--- src/main/java/com/example/employee/entity/Company.java
+++ src/main/java/com/example/employee/entity/Company.java
@@ -1,5 +1,58 @@
 package com.example.employee.entity;
 
-public class Company {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
+@Entity
+public class Company implements Serializable {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(nullable = false)
+    private long id;
+    @Column(nullable = false)
+    private String companyName;
+    @Column(nullable = false)
+    private Integer employeesNumber;
+    public Company(){};
+
+    public Company(long id, String companyName, Integer employeesNumber){
+        this.id = id;
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Integer getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(Integer employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
+
+    @Override
+    public String toString() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", this.id);
+        map.put("name", this.companyName);
+        map.put("age", this.employeesNumber);
+        return map.toString();
+    }
 }
--- src/main/java/com/example/employee/entity/Employee.java
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -1,5 +1,112 @@
 package com.example.employee.entity;
 
-public class Employee {
+import org.springframework.data.annotation.Id;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+@Entity
+public class Employee implements Serializable {
+    @javax.persistence.Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(nullable = false)
+    private long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private Integer age;
+    @Column(nullable = false)
+    private String gender;
+//    @Column(nullable = false)
+//    private long companyId;
+    @Column(nullable = false)
+    private Integer salary;
+
+    @ManyToOne
+    @JoinColumn(name = "companyId", nullable = false)
+    private Company company;
+
+    public Employee() {
+    }
+
+    public Employee(String name, Integer age, String gender,Integer salary, long companyId, long id) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.gender = gender;
+//        this.companyId = companyId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Employee setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Employee setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Employee setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Employee setGender(String gender) {
+        this.gender = gender;
+        return this;
+    }
+
+//    public long getCompanyId() {
+//        return companyId;
+//    }
+//
+//    public void setCompanyId(long companyId) {
+//        this.companyId = companyId;
+//    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", this.id);
+        map.put("name", this.name);
+        map.put("age", this.age);
+        map.put("gender", this.gender);
+        return map.toString();
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
 }
